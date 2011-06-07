7ecf6952875619e15c38599e5f383b0e74435b14
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-07 18:41:56 +0100

    Added "I'm so C, I point to myself"

---
+++ im_so.c
@@ -0,0 +1,18 @@
+/**
+ * Name: I'm so C, I point to myself!
+ * Reason: The name coming up as a joke in a conversation
+ * Original Implementation Date: 7th June 2011
+ *
+ * Piece of code that gets stuck in an infinite dereferencing loop
+ */
+#include "stdio.h"
+ 
+void main(void)
+{
+	void* p = (void *)&p;
+	while (p)
+	{
+		printf("%i\n", (int)p);
+		p = (*(void **)p);
+	}
+}
