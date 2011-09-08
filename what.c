96aabc91ff511e1e4fbed04e86d4343f423e4a38
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-09-08 11:16:50 +0100

    Added "what.c"

---
+++ what.c
@@ -0,0 +1,27 @@
+/*
+ * A demonstration of the beauty of C.
+ * This should compile on any x86-32 Linux installation with no-execute bit
+ * support disabled, using GCC with default options.
+ *
+ * You can tell it's valid C because it begins with "int main".
+ * If this isn't clear, take a look at:
+ * http://www.doc.ic.ac.uk/~msr08/things/what_hint.c
+ *
+ * (c) 2010 Mark Rutland <msr08@doc.ic.ac.uk>
+ */
+int main[] =
+{
+	443,
+	309248,
+	-494338048,
+	2664,
+	1819437056,
+	1869095268,
+	1752127264,
+	1819043144,
+	-511057367,
+	716931277,
+	-1207959552,
+	1,
+	322404557,
+};
